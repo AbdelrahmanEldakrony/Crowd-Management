e9300b27c3334dd4a112fd9ef941d1c413eb334b
abdelrahman El-dakrony <abdelrahmaneldakrony@gmail.com>
2018-08-02 05:03:24 +0200

    Face Recognition & C# Module

---
+++ Hajj/Hajj/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hajj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string progToRun = @"C:\Users\dell\Desktop\recc\Face-Recognition\face_recognition.py";
+           // char[] spliter = { '\r' };
+            Process proc = new Process();
+            proc.StartInfo.FileName = @"C:\Users\dell\AppData\Local\Programs\Python\Python36\python.exe";
+            proc.StartInfo.RedirectStandardOutput = true;
+
+            proc.StartInfo.UseShellExecute = false;
+            
+            proc.StartInfo.Arguments = string.Concat(progToRun);
+            proc.Start();
+
+            StreamReader sReader = proc.StandardOutput;
+            string output = sReader.ReadToEnd();
+
+            
+                Console.WriteLine(output);
+
+            proc.WaitForExit();
+
+            Console.ReadLine();
+        }
+    }
+}
